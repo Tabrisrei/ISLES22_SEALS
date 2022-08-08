b6ee0010b0ba29bb52008c8735ee81d23a5b2ff5
高圣博 <gaoshengbo@gaoshengbodeMacBook-Air.local>
2022-08-08 20:06:13 +0800

    repo init

---
+++ README.md
@@ -0,0 +1,65 @@
+# Property data preprocessing and nnUNet for efficient Acute Ischemic Stroke segmentation
+
+## Introduction
+
+This is the repository of team SEALS submission for ISLES22 competition.
+
+## Installation
+
+This repository is based on nnUNet. Please follow the installation instruction of nnUNet. 
+We recommend to install conda environment before installing this repository.
+We will provide the installation instruction of this repo.
+
+## Usage
+
+If you do not want to install conda, please skip to the step 3.(We do not recommend to install nnUNet straightly.)
+
+1. Let us hypothesize you are using conda environment named "nnunet", activate conda environment "nnunet"
+
+```bash
+conda activate nnunet
+```
+
+2. Run the following command to install basic pytorch、torchvision library and corresponding cudatoolkit.
+
+```bash
+conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
+```
+
+3. Clone this repository.
+
+```bash
+git clone https://github.com/Tabrisrei/ISLES22_SEALS.git
+```
+
+4. Run the following command to install nnUNet.
+
+```bash
+cd ISLES22_SEALS
+pip install -e .
+```
+
+If successful installed all required packages, you can run the following command to test your own dataset.
+
+5. Download the packed model file from XXX, unpack it and put in the directory "~/ISLES22_SEALS/data/nnUNet_trained_models".
+
+6. Convert your dwi and adc image to mha format.
+
+7. Put your dwi and adc data in `/input/images/dwi-brain-mri/` and `/input/images/adc-brain-mri/` folder respectively.
+
+8. Run the following command to reproduce the results.
+
+```shell
+bash nnunet_launcher.sh
+```
+
+After the results are generated, you can check the results in `/output/images/stroke-lesion-segmentation/` folder.
+
+## Questions
+
+Please contact gtabris@buaa.edu.cn
+
+## Acknowledgement
+
+- This code is adapted from [nnUNet](https://github.com/MIC-DKFZ/nnUNet)
+- We thank Dr. Fabian Isensee etc. for their elegant and efficient code base.
---
+++ nnunet_launcher.sh
@@ -0,0 +1,112 @@
+export nnUNet_raw_data_base="data/nnUNet_raw_data_base"
+export nnUNet_preprocessed="data/nnUNet_preprocessed"
+export RESULTS_FOLDER="data/nnUNet_trained_models"
+export nnUNet_n_proc_DA=24
+
+python nnunet/dataset_conversion/Task500_Ischemic_Stroke_Test.py
+
+nnUNet_change_trainer_class -i data/nnUNet_trained_models/nnUNet/3d_fullres/Task012_Ischemic_Stroke_TM_Fullset/nnUNetTrainerV2_DDP__nnUNetPlansv2.1 \
+                            -tr nnUNetTrainerV2 \
+
+CUDA_VISIBLE_DEVICES=0 \
+nnUNet_predict \
+               -i $nnUNet_raw_data_base/nnUNet_raw_data/Task500_Ischemic_Stroke_Test/imagesTs/ \
+               -o test_result/preliminary_phase/fold0 \
+               -t 12 \
+               -tr nnUNetTrainerV2_DDP \
+               -m 3d_fullres \
+               -f 0 \
+               -z \
+               --disable_postprocessing  
+
+CUDA_VISIBLE_DEVICES=0 \
+nnUNet_predict \
+               -i $nnUNet_raw_data_base/nnUNet_raw_data/Task500_Ischemic_Stroke_Test/imagesTs/ \
+               -o test_result/preliminary_phase/fold1 \
+               -t 12 \
+               -tr nnUNetTrainerV2_DDP \
+               -m 3d_fullres \
+               -f 1 \
+               -z \
+               --disable_postprocessing
+
+CUDA_VISIBLE_DEVICES=0 \
+nnUNet_predict \
+               -i $nnUNet_raw_data_base/nnUNet_raw_data/Task500_Ischemic_Stroke_Test/imagesTs/ \
+               -o test_result/preliminary_phase/fold2 \
+               -t 12 \
+               -tr nnUNetTrainerV2_DDP \
+               -m 3d_fullres \
+               -f 2 \
+               -z \
+               --disable_postprocessing
+               
+CUDA_VISIBLE_DEVICES=0 \
+nnUNet_predict \
+               -i $nnUNet_raw_data_base/nnUNet_raw_data/Task500_Ischemic_Stroke_Test/imagesTs/ \
+               -o test_result/preliminary_phase/fold3 \
+               -t 12 \
+               -tr nnUNetTrainerV2_DDP \
+               -m 3d_fullres \
+               -f 3 \
+               -z \
+               --disable_postprocessing
+
+CUDA_VISIBLE_DEVICES=0 \
+nnUNet_predict \
+               -i $nnUNet_raw_data_base/nnUNet_raw_data/Task500_Ischemic_Stroke_Test/imagesTs/ \
+               -o test_result/preliminary_phase/fold4 \
+               -t 12 \
+               -tr nnUNetTrainerV2_DDP \
+               -m 3d_fullres \
+               -f 4 \
+               -z \
+               --disable_postprocessing
+
+python recover_softmax.py \
+                        -i test_result \
+                        -o test_result_recover/preliminary_phase/fold0 \
+                        -m preliminary_phase \
+                        -f fold0
+
+python recover_softmax.py \
+                        -i test_result \
+                        -o test_result_recover/preliminary_phase/fold1 \
+                        -m preliminary_phase \
+                        -f fold1
+
+python recover_softmax.py \
+                        -i test_result \
+                        -o test_result_recover/preliminary_phase/fold2 \
+                        -m preliminary_phase \
+                        -f fold2                      
+
+python recover_softmax.py \
+                        -i test_result \
+                        -o test_result_recover/preliminary_phase/fold3 \
+                        -m preliminary_phase \
+                        -f fold3
+
+python recover_softmax.py \
+                        -i test_result \
+                        -o test_result_recover/preliminary_phase/fold4 \
+                        -m preliminary_phase \
+                        -f fold4
+
+model_0=test_result_recover/preliminary_phase/fold0
+model_1=test_result_recover/preliminary_phase/fold1
+model_2=test_result_recover/preliminary_phase/fold2
+model_3=test_result_recover/preliminary_phase/fold3
+model_4=test_result_recover/preliminary_phase/fold4
+python -m ensemble_predictions -f $model_0 \
+                                  $model_1 \
+                                  $model_2 \
+                                  $model_4 \
+                                  $model_5 \
+                                -o test_ensemble/ \
+                                --npz
+
+python threshold_redirect.py \
+                            -i test_ensemble/ \
+                            -o output/images/stroke-lesion-segmentation/
+
